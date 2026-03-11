86139eadf97880750ae38e5659bcf700006869a7
H82M19\Student <Student>
2026-03-11 11:14:22 +0700

    fetch api error

---
+++ fetch_api/src/App.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  var url = "https://jsonplaceholder.typicode.com/users2";
+  // useEffect(()=>{
+  //   var url = "https://jsonplaceholder.typicode.com/users";
+  //   var fetchdata = fetch(url);
+  //   var res = fetchdata.then((response)=>{
+  //       return response.json();
+  //   }).then((data) =>{
+  //       setData(data);
+  //       console.log(data);
+  //   })
+  // },[])
+
+  useEffect(()=>{
+    async function fetchdata() {
+      try {
+        var res = await fetch(url);
+        if(!res.ok){
+          throw new Error("api error");
+        }
+        var data = await res.json();
+
+        console.log(data);
+        setData(data);
+
+      } catch (error) {
+        setError(error.message);
+      }finally{
+        setTimeout(()=>{
+          setLoading(false);
+        },1000)
+      }
+    }
+    fetchdata();
+
+  },[])
+
+  if(error){
+    return(
+      <div>
+        {error}
+      </div>
+    )
+  }
+
+  return (
+    <>
+      {loading? (
+        <p>loading.....</p> 
+      ) : (
+        data.map((item)=>{
+          return <div key={item.id}>
+            <p>{item.name}</p>
+            <p>{item.email}</p>
+          </div>
+        })
+      )}
+    </>
+  )
+}
+
+export default App
+
